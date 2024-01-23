ecee3b0f2c06d1a0e83c7cb795347fbe246af60b
EliAs <EliAs@DESKTOP-K95LG4P>
2024-01-23 18:25:21 +0330

    Add MediatR & Dapper To Application Layer

--- IDP.Api/Program.cs
+++ IDP.Api/Program.cs
@@ -7,7 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-
 builder.Services.AddAuthentication(op => 
 {
     op.DefaultSignOutScheme = JwtBearerDefaults.AuthenticationScheme;
